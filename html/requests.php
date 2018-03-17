1c08475bbf05286c6cd20aaab0f4de9158663207
Ryan Felton <RyanDillinFelton@gmail.com>
2018-03-17 23:42:12 +0000

    some stuff

--- html/requests.php
+++ html/requests.php
@@ -22,29 +22,41 @@
 		if (!$userID = $_SESSION["userid"]) {
 			echo "not logged in";
 		} else {
-
-			$sql = "SELECT * FROM requests WHERE ID = '$userID';";
+			$sql = "SELECT * FROM requests JOIN users ON requests.userID=users.ID;";
 			$result = "";
 
 			if ($result = $conn->query($sql)) {
 				//get the first fetched row
-			    $row = $result->fetch_assoc();
+			    while($row = $result->fetch_assoc()){
+			    	//set variables with result
+			    	$rid = $row["ID"];
+				    $title = $row["Title"];
+				    $description = $row["Description"];
+				    $foodPref = $row["FoodPref"];
+				    $dietaryNeeds = $row["DietaryNeeds"];
+				    $fname = $row["FirstName"];
+				    $lname = $row["LastName"];
+
+				    echo "<a href=requestDetails.php?rid=$rid>"
+				    echo "<div style='border:solid black; margin: auto; width:60%;'>";
+				    echo "<h1>$title by $fname $lname</h1><br>";
+				    echo "<p>$description</p><br>";
+				    echo "<p>$foodPref</p><br>";
+				    echo "<p>$dietaryNeeds</p><br>";
+				    echo "</div>";
+
+			    }
 
 			    //set variables with result
-			    $fname = $row["FirstName"];
-			    $lname = $row["LastName"];
-			    $country = $row["country"];
-			    $state = $row["State"];
-			    $city = $row["City"];
+			    $title = $row["Title"];
+			    $description = $row["Description"];
+			    $foodPref = $row["FoodPref"];
+			    $dietaryNeeds = $row["DietaryNeeds"];
+
 			} else {
 			    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
 			}
 			$conn->close();
-
-			//page content
-			echo "<h1>Account page</h1></br>";
-			echo "<h1>$fname $lname </h1></br>";
-			echo "<h2>Location: $country, $state, $city</h2><br><br>";
 		}
 	?>
 </body>
